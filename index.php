977764dc18d95d3fcd12b60afe8d22ed9120dc40
squidera <37768616+squidera@users.noreply.github.com>
2018-03-25 13:45:30 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<center> <h1> Welcome To our openshift DevOps(CI/CD) project</h1> </center>
+
+<?php
+
+echo "Hi Param how have you been ? :-) This is a new code";
+
+?>
+
+</body>
+</html>
